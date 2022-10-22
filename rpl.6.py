e44f0aab2b5bb9d93f2a1ce9bd7c80daddeab8f7
Rizkyadiwijaya <115349035+Rizkyadiwijaya@users.noreply.github.com>
2022-10-22 19:08:51 +0700

    Add files via upload

---
+++ rpl.6.py
@@ -0,0 +1,14 @@
+nilai = 85
+
+if(nilai>90):
+    print("nilai A")
+elif(nilai <=85):
+    print("nilai B")
+elif(nilai <=80):
+    print("nilai C")
+elif(nilai <=70):
+    print("nilai D")
+elif(nilai <=60):
+    print("nilai E")
+else:
+    print("anda mengulang")
