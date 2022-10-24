ece1fb0289853106b6a96411e6194526b8e17233
Rizkyadiwijaya <115349035+Rizkyadiwijaya@users.noreply.github.com>
2022-10-24 13:35:52 +0700

    Add files via upload

---
+++ rpl.8.py
@@ -0,0 +1,7 @@
+username = input("masukkan username :")
+password = input("masukkan password :")
+
+if username =="bro" or password =="sebentar":
+    print("anda berhasil login")
+else:
+    print("gagal login")
