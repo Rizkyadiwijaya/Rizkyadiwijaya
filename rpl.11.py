1f3df5d9c938c61309731d6e18083dcffb73f2b9
Rizkyadiwijaya <115349035+Rizkyadiwijaya@users.noreply.github.com>
2022-10-27 19:09:10 +0700

    Add files via upload

---
+++ rpl.11.py
@@ -0,0 +1,15 @@
+#indexing
+
+nama_depan = "Rizky"
+nama_tengah = "adi"
+nama_terakhir = "wijaya"
+
+nama_lengkap = nama_depan + nama_tengah + nama_terakhir
+print(nama_lengkap)
+
+print("index ke-0 : " + nama_lengkap[0])
+print("index ke-4 : " + nama_lengkap[5])
+print("index ke-8 : " + nama_lengkap[9])
+print("index ke-(-1) : " + nama_lengkap[-1])
+print("index ke-(-5) : " + nama_lengkap[-6])
+
