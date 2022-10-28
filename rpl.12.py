ace4dadcd0db85bcf869b578ff2e7671111cd58c
Rizkyadiwijaya <115349035+Rizkyadiwijaya@users.noreply.github.com>
2022-10-28 19:47:31 +0700

    Add files via upload

---
+++ rpl.12.py
@@ -0,0 +1,10 @@
+panjang = int(input("masukkan panjang : "))
+lebar = int(input("masukkan lebar : "))
+
+luas = panjang*lebar
+
+if(luas > 200):
+    print("Tanah sangat luas")
+else :
+    print("Tanah cukup luas")
+
