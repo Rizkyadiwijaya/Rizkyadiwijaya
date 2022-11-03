171cbe57207f3cda75492ab9474e5462a485491b
Rizkyadiwijaya <115349035+Rizkyadiwijaya@users.noreply.github.com>
2022-11-03 21:13:22 +0700

    Add files via upload

---
+++ rpl.18.py
@@ -0,0 +1,13 @@
+nilai = 0
+
+print(f"nilai sekarang {nilai}")
+
+while nilai < 6:
+    nilai += 1
+    print(f"nilai sekarang {nilai}")
+
+    if nilai == 6:
+        print("nice!!!")
+        print("jujur ngelag!!")
+    
+    print("sampai sini mo dulu!!")
